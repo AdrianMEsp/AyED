1e0c3037beaf0d2cf3c088ffc543a754ffcab565
Adrian Espindola <103772656+AdrianMEsp@users.noreply.github.com>
2022-11-23 22:39:41 -0300

    Update DFS.java

--- RecorridosGrafos/DFS.java
+++ RecorridosGrafos/DFS.java
@@ -1,65 +1,69 @@
-public <T> void dfs(GRafo<T> grado, T dato){
- //buscar el vertice que tiene el dato
-  Vertice<T> vertice;
-  boolean [] visitados = new [grafo.listaDeVertices().tamanio()];
-  ListaEnlazadaGenerica<Vertice<T>> vertices = Grafo.listaDeVertices();
-  vertices.comenzar();
-  while(!vertices.fin()){
-   vertices=vertices.proximo;
-    if(vertice.dato().equals(dato)){
-      break;
-    }
-  }
-  if (vertice != null){
-    this.dfs_private(grafo,vertice);
-  }
-}
+public class EP{
 
-private void dfs_private(Grafo<T> grado, Vertice<T> vertice; boolean[] visitados){
- //procesar el dato
-  System.out.println(vertice.dato());
-  
-  //llamar a recurcion
-  ListaEnlazadaGenerica<Arista<T>> arista = grafo.listaDeAdyacentes(vertice);
-  aristas.comenzar();
-  while(!aristas.fin()){
-   Arista<T> arista = aristas.proximo();
-    Vertice<T> vDest = arista.verticeDestino();
-    if(!visitados[vDest.posicion()])
-      this.dfs_private(grafo,vDest,visitados);
-  }
-  
-  visitados[vertice.posicion] = false;
-}
-//linea-55
-
-///me faltan lineas aca
-
-//linea-91
-ColaGenerica<Vertice<T>> cola;
-cola.encolar(vertice);
-cola.enconlar(null);
-
-while (!cola.esVacia()){
- Vertice<T> vAux=cola.desencolar();
- if(vAux != null){
-  System.out.println(vAux.dato());
-  
-  ListaEnlazadaGenerica<Arista<T>> aristas = grafo.listaDeAdyacentes(vAux);
-  aristas.comenzar();
-  while(!aristas.fin()){
-   Arista<T> arista = aristas.proximo();
-   Vertice<T> vDest = arista.verticeDestino();
-   
-   if(!visitados[vDest.posicion()]){
-    visitados[vDest.posicion()] = true;
-    cola.encolar(vDest);
-    
-  }
+ public <T> void dfs(GRafo<T> grado, T dato){
+  //buscar el vertice que tiene el dato
+   Vertice<T> vertice;
+   boolean [] visitados = new [grafo.listaDeVertices().tamanio()];
+   ListaEnlazadaGenerica<Vertice<T>> vertices = Grafo.listaDeVertices();
+   vertices.comenzar();
+   while(!vertices.fin()){
+    vertices=vertices.proximo;
+     if(vertice.dato().equals(dato)){
+       break;
+     }
+   }
+   if (vertice != null){
+     this.dfs_private(grafo,vertice);
+   }
  }
-}else{
-  if (!cola.esVacia()){
-   cola.encolar(null);
+
+ private void dfs_private(Grafo<T> grado, Vertice<T> vertice; boolean[] visitados){
+  //procesar el dato
+   System.out.println(vertice.dato());
+
+   //llamar a recurcion
+   ListaEnlazadaGenerica<Arista<T>> arista = grafo.listaDeAdyacentes(vertice);
+   aristas.comenzar();
+   while(!aristas.fin()){
+    Arista<T> arista = aristas.proximo();
+     Vertice<T> vDest = arista.verticeDestino();
+     if(!visitados[vDest.posicion()])
+       this.dfs_private(grafo,vDest,visitados);
+   }
+
+   visitados[vertice.posicion] = false;
+ }
+ //linea-55
+
+ ///me faltan lineas aca
+
+ //linea-91
+ ColaGenerica<Vertice<T>> cola;
+ cola.encolar(vertice);
+ cola.enconlar(null);
+
+ while (!cola.esVacia()){
+  Vertice<T> vAux=cola.desencolar();
+  if(vAux != null){
+   System.out.println(vAux.dato());
+
+   ListaEnlazadaGenerica<Arista<T>> aristas = grafo.listaDeAdyacentes(vAux);
+   aristas.comenzar();
+   while(!aristas.fin()){
+    Arista<T> arista = aristas.proximo();
+    Vertice<T> vDest = arista.verticeDestino();
+
+    if(!visitados[vDest.posicion()]){
+     visitados[vDest.posicion()] = true;
+     cola.encolar(vDest);
+
+   }
   }
-  
-  //linea-117
+ }else{
+   if (!cola.esVacia()){
+    cola.encolar(null);
+   }
+
+   //linea-117
+
+ }
