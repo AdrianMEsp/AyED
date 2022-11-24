78b02e33fca207a51b2d68cdcd63f65a0a5fcd9e
Adrian Espindola <103772656+AdrianMEsp@users.noreply.github.com>
2022-11-23 22:55:37 -0300

    Update DFS2.java

--- RecorridosGrafos/DFS2.java
+++ RecorridosGrafos/DFS2.java
@@ -14,6 +14,47 @@ public class EP{
      }
    }
    if (vertice != null){
-     this.dfs_private(grafo,vertice,visitados);
+     this.dfs_private(grafo,vertice,visitados,datoB,camino,temporal);
    }
+ }
+
+ private <T> void dfs_private(
+   Grafo<T> grafo,
+   Vertices<T> vertice,
+   boolean[] visitados,
+   T datoB,
+   ListaGenerica<T> camino,
+   ListaGenerica<T> temporal){
+   
+   visitados[vertice.getPosicion()] = true;
+   temporal.agregarFinal(vertice.dato());
+   
+   //procesar el dato
+   if(vertice.dato().equals(datoB) ){
+     camino.comenzar();
+     while(!camino.fin()){
+      
+       camino.eliminarEn(camino.tamanio());
+       camino.proximo();
+     }
+     //actualizo la lista camino con la temporal
+     
+     temporal.comenzar();
+     while(!temporal.fin()){
+      camino.agregarFinal(temporal.proximo()); 
+     }
+      
+   }else {
+     //llamar recursivamente
+     ListaEnlazadaGenerica<Aristas<T>> aristas = grafo.listaDeAdyacentes(vertice);
+     aristas.comenzar();
+     while(!aristas.fin()){
+      Arista<T> arista = aristas.proximo();
+      Vertice<T> vDest = arista.verticeDestino();
+      if(!visitados[vDest.posicion()])
+        this.dfs_private(grafo,vDest,visitados,datoB,camino,temporal);
+   }
+     
+   visitados[vertice.posicion] = false;
+   temporal.eliminarEn(temporal.tamanio());
  }
