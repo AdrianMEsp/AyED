183a266b7964c9d259846727deba9002e180ea81
Adrian Espindola <103772656+AdrianMEsp@users.noreply.github.com>
2022-11-23 23:04:34 -0300

    Update DFS.java

--- RecorridosGrafos/DFS.java
+++ RecorridosGrafos/DFS.java
@@ -3,17 +3,17 @@ public class EP{
  public <T> void dfs(GRafo<T> grado, T dato){
   //buscar el vertice que tiene el dato
    Vertice<T> vertice;
-   boolean [] visitados = new [grafo.listaDeVertices().tamanio()];
+   boolean [] visitados = new boolean [grafo.listaDeVertices().tamanio()];
    ListaEnlazadaGenerica<Vertice<T>> vertices = Grafo.listaDeVertices();
    vertices.comenzar();
    while(!vertices.fin()){
-    vertices=vertices.proximo;
+    vertices=vertices.proximo();
      if(vertice.dato().equals(dato)){
        break;
      }
    }
    if (vertice != null){
-     this.dfs_private(grafo,vertice);
+     this.dfs_private(grafo,vertice,visitados);
    }
  }
 
@@ -26,9 +26,9 @@ public class EP{
    aristas.comenzar();
    while(!aristas.fin()){
     Arista<T> arista = aristas.proximo();
-     Vertice<T> vDest = arista.verticeDestino();
-     if(!visitados[vDest.posicion()])
-       this.dfs_private(grafo,vDest,visitados);
+    Vertice<T> vDest = arista.verticeDestino();
+    if(!visitados[vDest.posicion()])
+     this.dfs_private(grafo,vDest,visitados);
    }
 
    visitados[vertice.posicion] = false;
